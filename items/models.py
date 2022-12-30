4cda6e39b4333a0d05a7b56e1fde1d09a6ac4485
Chetram Bassit <chetrambassit121@gmail.com>
2022-12-30 08:21:57 -0500

    created serializers.py, created Item and User serializers

--- items/models.py
+++ items/models.py
@@ -2,24 +2,24 @@ from django.db import models
 
 
 """
-Using class to create an Item object 
+Creating an Item Model
 """
 class Item(models.Model):
     
     """
-    Creating fields to define Item object
+    Creating fields to define Item Model
     """
     name = models.CharField(max_length=100, blank=False, default='')
     created = models.DateTimeField(auto_now_add=True)
 
     """
-    Inner class, defaulting all Item objects to display in descending order
+    Inner class, defaulting all Item objects (models) to display in descending order
     """
     class Meta:
         ordering = ['created']
 
     """
-    Defining save method, saving all arguements and keyword arguements
+    Defining save method, saving all arguments and keyword arguments
     """
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)

b871ade4f4d0733a93649e82b33ea93290f8e180
Chetram Bassit <chetrambassit121@gmail.com>
2022-12-30 08:11:30 -0500

    created Item Model, added to admin.py

--- items/models.py
+++ items/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+"""
+Using class to create an Item object 
+"""
+class Item(models.Model):
+    
+    """
+    Creating fields to define Item object
+    """
+    name = models.CharField(max_length=100, blank=False, default='')
+    created = models.DateTimeField(auto_now_add=True)
+
+    """
+    Inner class, defaulting all Item objects to display in descending order
+    """
+    class Meta:
+        ordering = ['created']
+
+    """
+    Defining save method, saving all arguements and keyword arguements
+    """
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
